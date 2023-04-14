d6407cd8ed4f4b26587b8f1c7ab968eae93010bd
James R Harmon <115373192+jrh079@users.noreply.github.com>
2023-04-14 11:29:38 -0500

    Update arpdetect-2.py

--- arpdetect-2.py
+++ arpdetect-2.py
@@ -33,7 +33,7 @@ anykey = input('Press Anykey to continue')
 print("")
 print("")
 
-
+# Mac Check function
 def mac(ipaddress):
     arp_request = scapy.ARP(pdst=ipaddress)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -41,22 +41,24 @@ def mac(ipaddress):
     answerlist = scapy.srp(arp_req_br, iface="wlan0", timeout=5, verbose=False)[0]
     if answerlist == "":
         return answerlist[0][1].hwsrc
-
+# Interface sniff function without set timeout which may be adjusted
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, timeout=10)
-
+# function to read and do checks.
 def process_sniffed_packet(packet):
     if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
         originalmac = mac(packet[scapy.ARP].psrc)
         responsemac = packet[scapy.ARP].hwsrc
-
+# Check to see if MAC's have been altered / positive signature
         if originalmac != responsemac:
             print("ARP Attack Detected ")
             #anykey = input('Press Anykey to continue')
             print(' [ ' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))+ ' ] '+  ' <<ARP Attack Detected>> ')
             print('Running 5 second tshark capture for Admin review ')
+ # Call Tshark for capture and Admin Review           
             os.system("tshark -i wlan0 -a duration:5 -w /home/kali/temp/arpdetect.pcap")
             os.system("chmod a+rw /home/kali/temp/arpdetect*.pcap")
+ # Modifies output to make it easier to work with post capture.
             anykey = input('Press Anykey to Exit')
             quit()
              

6b9f04ddd0a9bf6da35ff99dc40349185aec2064
James R Harmon <115373192+jrh079@users.noreply.github.com>
2023-04-14 10:54:36 -0500

    Update twindetect.py

--- twindetect.py
+++ twindetect.py
@@ -25,7 +25,7 @@ def check_packet(packet):
                 AP_MACs.add(packet.addr2)
                 print(f"New AP found: {packet.info.decode()} with MAC address {packet.addr2}")
         elif packet.type == 2 and packet.subtype == 0:
-            # This is a data frame
+            # Check mgmt / Beacon Frames
             if packet.addr2 in AP_MACs and packet.addr1 not in AP_MACs:
                 print(f"Evil Twin Rogue AP detected! A client with MAC address {packet.addr2} connected to {packet.info.decode()} with MAC address {packet.addr1} which is not in our list of known APs.")
 
